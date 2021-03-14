85f723dac67a66f8e79cc029b664d6402ad9842e
Enrique <erivera7240@gmail.com>
2021-03-14 17:22:33 -0500

    font added to scence

--- 13-3d-text/src/script.js
+++ 13-3d-text/src/script.js
@@ -28,14 +28,29 @@ const textureLoader = new THREE.TextureLoader()
  */
  const fontLoader = new THREE.FontLoader()
 
- fontLoader.load(
-     '/fonts/helvetiker_regular.typeface.json',
-     (font) =>
-     {
-         console.log('loaded')
-     }
- )
-
+fontLoader.load(
+    '/fonts/helvetiker_regular.typeface.json',
+    (font) =>
+    {
+        const textGeometry = new THREE.TextGeometry(
+            'Hello Three.js', 
+            {
+                font: font,
+                size: 0.5,
+                height: 0.2,
+                curveSegments: 12,
+                bevelEnabled: true,
+                bevelThickness: 0.03,
+                bevelSize: 0.02,
+                bevelOffset: 0,
+                bevelSegments: 5
+            }
+        )
+        const textMaterial = new THREE.MeshBasicMaterial()
+        const text = new THREE.Mesh(textGeometry, textMaterial)
+        scene.add(text)
+    }
+)
 
 
  
